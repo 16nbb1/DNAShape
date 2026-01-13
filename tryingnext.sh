0f6f884267b9dec831e52be478d64a9ad297b57e
Nadejda Boev <nb.boev@mail.utoronto.ca>
2026-01-12 19:02:40 -0500

    Update tryingnext.sh

--- tryingnext.sh
+++ tryingnext.sh
@@ -58,3 +58,9 @@ nextflow run main.nf \
 
 #git --version
 git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/16nbb1/DNAShape.git
+git branch -M main
+git push -u origin main
+
